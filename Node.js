c6376022b82456fe0f2cbc340b426845d1f7124a
JuniorTIM <musa.aliev06@yandex.ru>
2022-05-23 14:22:20 +0300

    Модули прописаны

---
+++ Node.js
@@ -0,0 +1,8 @@
+import os from 'os';
+console.log(os.homedir())
+console.log(os.hostname())
+console.log(os.version())
+console.log(os.type())
+
+import welcome from './welcome.js'
+console.log(welcome())
